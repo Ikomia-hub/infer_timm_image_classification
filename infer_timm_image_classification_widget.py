ac77f425237f2b4583af42fbf246334ad7e5f47d
ambroise <ambroise.berthe@ikomia.com>
2022-06-20 17:08:27 +0200

    Refactor

--- infer_timm_image_classification_widget.py
+++ infer_timm_image_classification_widget.py
@@ -45,10 +45,10 @@ class InferTimmImageClassificationWidget(core.CWorkflowTaskWidget):
         self.gridLayout = QGridLayout()
 
         timm_models = timm.list_models(pretrained=True)
-        self.combo_model = Autocomplete(timm_models,parent=None, i=True, allow_duplicates=False)
+        self.combo_model = Autocomplete(timm_models, parent=None, i=True, allow_duplicates=False)
         self.label_model = QLabel("Model name")
-        self.gridLayout.addWidget(self.combo_model,0,1)
-        self.gridLayout.addWidget(self.label_model,0,0)
+        self.gridLayout.addWidget(self.combo_model, 0, 1)
+        self.gridLayout.addWidget(self.label_model, 0, 0)
         self.combo_model.setCurrentText(self.parameters.model_name)
         self.spin_input_h = pyqtutils.append_spin(self.gridLayout, "Input height", self.parameters.input_size[0],
                                                   min=16)
